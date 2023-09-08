ef10fc8571f718389a10a648e57f2c194fd21968
rebb11 <joserebomanguerra@gmail.com>
2023-09-08 13:58:04 +0800

    added multistep-form applicant profile

--- applicant/sidenav.php
+++ applicant/sidenav.php
@@ -8,7 +8,7 @@
         <a href="find_jobs.php" <?php echo isActivePage("find_jobs.php"); ?>><i class="bi bi-file-post"></i>&nbsp;Find Jobs</a>
         <a href="urgent_hiring.php" <?php echo isActivePage("urgent_hiring.php"); ?>><i class="bi bi-search"></i>&nbsp;Urgent Hiring</a>
         <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-bell" style="font-size:20px"></i>&nbsp;Applicant</a>
-        <a href="personal_info.php" <?php echo isActivePage("personal_info.php"); ?>><i class="bi bi-person-lines-fill"></i>&nbsp;Profile</a>
+        <a href="multiform_profile.php" <?php echo isActivePage("multiform_profile.php"); ?>><i class="bi bi-person-lines-fill"></i>&nbsp;Profile</a>
         <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-sliders2"></i>&nbsp;More Details</a>
         </div>
 </div>
