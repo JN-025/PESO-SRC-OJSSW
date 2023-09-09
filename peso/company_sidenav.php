a958974c6d9caa9f96659a97228d11a8fa35cf80
JN-025 <jncamposano025@gmail.com>
2023-09-09 09:03:30 +0800

    approval button & company approval

--- peso/company_sidenav.php
+++ peso/company_sidenav.php
@@ -5,9 +5,9 @@
     <div class="sidenav">
         <div class="col-side">
         <a href="company_homepage.php" <?php echo isActivePage("company_homepage.php"); ?>><i class="bi bi-house-door-fill"></i>&nbsp;Home</a>
-        <a href="jobposting.php" <?php echo isActivePage("jobposting.php"); ?>><i class="bi bi-arrow-up-square-fill"></i>&nbsp;Job Posting</a>
-        <a href="jobpost.php" <?php echo isActivePage("jobpost.php"); ?>><i class="bi bi-file-earmark-spreadsheet"></i>&nbsp;Posted Jobs</a>
-        <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-person-vcard" style="font-size:20px"></i>&nbsp;Applications</a>
+        <a href="company_approval.php" <?php echo isActivePage("company_approval.php"); ?>><i class="bi bi-arrow-up-square-fill"></i>&nbsp;Approval</a>
+        <a href="Wcompany.php" <?php echo isActivePage("Wcompany.php"); ?>><i class="bi bi-file-earmark-spreadsheet"></i>&nbsp;Walk-in</a>
+        <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-person-vcard" style="font-size:20px"></i>&nbsp;Online</a>
         <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-gear-fill"></i>&nbsp;Settings</a>
         <a href="#" <?php echo isActivePage("#"); ?>><i class="bi bi-info-circle"></i>&nbsp;More Details</a>
         </div>
