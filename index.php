799a6a7c42326fb9dc0399a776b5677ca961051d
JN-025 <jncamposano025@gmail.com>
2023-09-15 01:04:36 +0800

    landing page (not done yet)

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="shortcut icon" href="assets/img/peso.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/landingpage.css">
+</head>
+<body>
+    <div class="main">
+        <div class="topnav">
+            <div class="peso-logo"></div>
+            <div class="right-corner">
+                <button class="login" onclick="document.location='#'">Log In</button>
+                <button class="signup" onclick="document.location='#'">Sign Up</button>
+            </div>
+        </div>
+        <div class="row-1">
+            <center>
+            <div class="col-1">
+                <div class="search-img" style="float: right;"></div>
+                <div class="search-box">
+                    <h1>SEARCH FOR A JOB</h1>
+                    <input type="text" name="" class="search-engine-1" placeholder="Skills, Company, or Job Title">
+                    <input type="text" name="" class="search-engine-2" placeholder="Location">
+                    <input type="text" name="" class="search-engine-3" placeholder="Experience">
+                    <br>
+                    <button name="search" class="button-search"><i class="bi bi-search" style="margin: 0 5px;"></i>Search</button>
+
+                </div>
+            </div>
+            </center>
+
+
+        </div>
+        <center>
+        <div class="row-2">
+            <h2>What is PESO?</h2>
+            <div class="col-2">
+                <div class="wordbox">
+                    <h3>The Public Employment Service Office (PESO) is a non-fee charging multi-dimenstional employment service facility or entity established in all Local Government Units (LGUs) in coordination with the Department of Labor and Employment (DOLE) pursuant to R.A. No. 8759 or the PESO Act of 1999 as amended by R.A. No. 10691.</h3>
+
+                </div>
+                <div class="logo-img">
+                    <img src="assets/img/peso.png" class="peso-logo">
+                </div>
+
+            </div>
+            
+            <div class="col-3">
+                <h4>Learn more about PESO Santa Rosa</h4>
+
+            </div>
+       \
+            
+
+        </div>
+        </center>
+    </div>
+</body>
+</html>
