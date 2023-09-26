f8f580f4d9a9ec846d4106d05ac064d81cc88a37
rebb11 <joserebomanguerra@gmail.com>
2023-09-26 13:19:46 +0800

    added new job posting (50% done)

--- company/job_posting.js
+++ company/job_posting.js
@@ -1,121 +1,47 @@
-/**
- * Define a function to navigate betweens form steps.
- * It accepts one parameter. That is - step number.
- */
-const navigateToFormStep = (stepNumber) => {
-    /**
-     * Hide all form steps.
-     */
-    document.querySelectorAll(".form-step").forEach((formStepElement) => {
-        formStepElement.classList.add("d-none");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const steps = document.querySelectorAll(".form-step");
+  const stepButtons = document.querySelectorAll(".btn-nav");
+
+  let currentStep = 0;
+
+  function showStep(stepIndex) {
+    steps.forEach((step, index) => {
+      if (index === stepIndex) {
+        step.classList.remove("d-none");
+      } else {
+        step.classList.add("d-none");
+      }
     });
-    /**
-     * Show the current form step (as passed to the function).
-     */
-    document.querySelector("#step-" + stepNumber).classList.remove("d-none");
-    /**
-     * Select the form step circle (progress bar).
-     */
-    const formStepCircle = document.querySelector('li[step="' + stepNumber + '"]');
-    /**
-     * Mark the current form step as active.
-     */
-    formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-completed");
-    formStepCircle.classList.add("form-stepper-active");
-    /**
-     * Loop through each form step circles.
-     * This loop will continue up to the current step number.
-     * Example: If the current step is 3,
-     * then the loop will perform operations for step 1 and 2.
-     */
-    for (let index = 0; index < stepNumber; index++) {
-        /**
-         * Select the form step circle (progress bar).
-         */
-        const formStepCircle = document.querySelector('li[step="' + index + '"]');
-        /**
-         * Check if the element exist. If yes, then proceed.
-         */
-        if (formStepCircle) {
-            /**
-             * Mark the form step as completed.
-             */
-            formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-active");
-            formStepCircle.classList.add("form-stepper-completed");
+  }
+
+  function handleStepNavigation(e) {
+    e.preventDefault();
+
+    const targetStep = parseInt(e.target.getAttribute("step_number"));
+
+    const currentStepFields = steps[currentStep].querySelectorAll("[required]");
+    let hasInvalidField = false;
+
+    for (const field of currentStepFields) {
+      if (!field.checkValidity()) {
+        field.reportValidity();
+        if (!hasInvalidField) {
+          field.focus();
+          hasInvalidField = true;
         }
+        break;
+      }
     }
-};
-/**
- * Select all form navigation buttons, and loop through them.
- */
-document.querySelectorAll(".btn-nav").forEach((formNavigationBtn) => {
-    /**
-     * Add a click event listener to the button.
-     */
-    formNavigationBtn.addEventListener("click", () => {
-        /**
-         * Get the value of the step.
-         */
-        const stepNumber = parseInt(formNavigationBtn.getAttribute("step_number"));
-        /**
-         * Call the function to navigate to the target form step.
-         */
-        navigateToFormStep(stepNumber);
-    });
-});
 
-// Get all the buttons with the class "scrollButton"
-const scrollButtons = document.querySelectorAll(".btn-nav");
+    if (!hasInvalidField) {
+      currentStep = targetStep;
+      showStep(currentStep);
+      console.log(currentStep);
+    }
+  }
 
-// Add a click event listener to each button
-scrollButtons.forEach((button) => {
-  button.addEventListener("click", function () {
-    // Scroll to the top of the page (body element)
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  stepButtons.forEach((button) => {
+    button.addEventListener("click", handleStepNavigation);
   });
 });
-
-
-
-//-------------------------------img------------------------------
-let dropArea = document.getElementById('drop-area')
-
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false)
-  })
-  
-  function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-
-  ;['dragenter', 'dragover'].forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight, false)
-  })
-  
-  ;['dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight, false)
-  })
-  
-  function highlight(e) {
-    dropArea.classList.add('highlight')
-  }
-  
-  function unhighlight(e) {
-    dropArea.classList.remove('highlight')
-  }
-
-  dropArea.addEventListener('drop', handleDrop, false)
-
-function handleDrop(e) {
-  let dt = e.dataTransfer
-  let files = dt.files
-
-  handleFiles(files)
-}
-
-
-
-
-
--- company/job_posting.php
+++ company/job_posting.php
@@ -16,41 +16,41 @@ include "../conn.php";
     include "topnav.php";
     ?>
     <div class="main-container">
-        <form action="" method="post">
+        <form id="form" action="" method="post">
             <div class="wrapper">
-                <section id="step-1" class="form-step" >
+                <section id="step-0" class="form-step" >
                     <label for="">Post a Job!</label>
                     <h2>Job Description</h2>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Job Title">
+                        <input class="description" id="jobTitle" name="jobTitle" type="text" placeholder="Job Title" required maxlength="50">
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Company Name">
+                        <input class="description" id="companyName" name="companyName" type="text" placeholder="Company Name" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Industry">
+                        <input type="text" placeholder="Industry" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Position">
+                        <input type="text" placeholder="Position" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Educational Background">
+                        <input type="text" placeholder="Educational Background" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Year of Experience">
+                        <input type="text" placeholder="Year of Experience" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Work Location">
+                        <input type="text" placeholder="Work Location" required>
                     </div>
                     <div class="form-col-1 double-col">
-                        <input type="text" placeholder="Salary">
-                        <input type="text" placeholder="Number of Slots">
+                        <input type="number" placeholder="Salary" required>
+                        <input type="number" placeholder="Number of Slots" required>
                     </div>
                     <div class="form-col-1">
-                        <input type="text" placeholder="Skills">
+                        <input type="text" placeholder="Skills" required>
                     </div>
                     <div style="width: 50%;margin-left: 5%; " class="form-col-1">
-                        <input type="text" placeholder="Type of Hiring">
+                        <input type="text" placeholder="Type of Hiring" required>
                     </div>
                     <div class="form-col-2">
                         <div class="flex-button">
@@ -58,30 +58,30 @@ include "../conn.php";
                         </div>
                         <div class="field-space"></div>
                         <div class="flex-button">
-                            <button class="btn-nav" step_number="2">Next</button>
+                            <button class="btn-nav" step_number="1">Next</button>
                         </div>
                     </div>
                 </section>
                 <!--Step 2-->
-                <section id="step-2" class="form-step d-none">
+                <section id="step-1" class="form-step d-none">
                     <label for="">Post a Job!</label>
                     <h2>Job Description</h2>
                     <h4>Describe the job position. Maximum of 250 words</h4>
                     <div class="form-col-1">
-                        <textarea class="description-box" name="description" placeholder="Job Posting Description" id="description" cols="30" rows="10" maxlength="250"></textarea>
+                        <textarea class="description-box" name="description" placeholder="Job Posting Description" id="description" cols="30" rows="10" maxlength="250" required></textarea>
                     </div>
                     <div class="form-col-2">
                         <div class="flex-button">
-                            <button step_number="1">Previous</button>
+                            <button class="btn-nav" step_number="0">Previous</button>
                         </div>
                         <div class="field-space"></div>
                         <div class="flex-button">
-                            <button step_number="3">Next</button>
+                            <button class="btn-nav" step_number="2">Next</button>
                         </div>
                     </div>
                 </section>
                 <!--Step 3-->
-                <section id="step-3" class="form-step d-none">
+                <section id="step-2" class="form-step d-none">
                     <label for="">Post a Job!</label>
                     <h2>Company Profile Picture</h2>
                     <h4>Note: Photo must be in PNG, JPEG, BMP, or, SVG.</h4>
@@ -89,7 +89,7 @@ include "../conn.php";
                     <div id="drop-area">
                     <form class="my-form">
                         <p>Upload multiple files with the file dialog or by dragging and dropping images onto the dashed region</p>
-                        <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)">
+                        <input type="file" id="fileElem" multiple accept="image/*" onchange="handleFiles(this.files)" required>
                         <label class="select" for="fileElem">Select some files</label>
                         <progress id="progress-bar" max=100 value=0></progress>
                     </form>
@@ -139,60 +139,60 @@ include "../conn.php";
                     </div>
                     <div class="form-col-2">
                         <div class="flex-button">
-                            <button class="btn-nav" step_number="2">Previous</button>
+                            <button class="btn-nav" step_number="1">Previous</button>
                         </div>
                         <div class="field-space"></div>
                         <div class="flex-button">
-                            <button class="btn-nav" step_number="4">Next</button>
+                            <button class="btn-nav" step_number="3">Next</button>
                         </div>
                     </div>
                 </section>
                 <!--Step 4-->
-                <section id="step-4" class="form-step d-none">
+                <section id="step-3" class="form-step d-none">
                     <label for="">Post a Jobs!</label>
                     <div class="form-col-1 question-container">
-                        <input type="text" placeholder="Question #1">
-                        <select name="" id="" required>
-                            <option value="Yes" selected hidden>Yes</option>
+                        <input class="question" id="questionNo1" name="questionNo1" type="text" placeholder="Question #1" required>
+                        <select name="" id="answerNo1" required>
+                            <option value="" selected hidden>Choose</option>
                             <option value="Yes">Yes</option>
                             <option value="No">No</option>
                         </select>
                     </div>
                     <div class="form-col-1 question-container">
-                        <input type="text" placeholder="Question #2">
-                        <select name="" id="" required>
-                            <option value="Yes" selected hidden>Yes</option>
+                        <input class="question" id="questionNo2" name="questionNo2" type="text" placeholder="Question #2" required>
+                        <select name="" id="answerNo2" required>
+                            <option value="" selected hidden>Choose</option>
                             <option value="Yes">Yes</option>
                             <option value="No">No</option>
                         </select>
                     </div>
                     <div class="form-col-1 question-container">
-                        <input type="text" placeholder="Question #3">
-                        <select name="" id="" required>
-                            <option value="Yes" selected hidden>Yes</option>
+                        <input class="question" id="questionNo3" name="questionNo3" type="text" placeholder="Question #3" required>
+                        <select name="" id="answerNo3" required>
+                            <option value="" selected hidden>Choose</option>
                             <option value="Yes">Yes</option>
                             <option value="No">No</option>
                         </select>
                     </div>
                     <div class="form-col-1 question-container">
-                        <input type="text" placeholder="Question #4">
-                        <select name="" id="" required>
-                            <option value="Yes" selected hidden>Yes</option>
+                        <input class="question" id="questionNo4" name="questionNo4" type="text" placeholder="Question #4" required>
+                        <select name="" id="answerNo4" required>
+                            <option value="" selected hidden>Choose</option>
                             <option value="Yes">Yes</option>
                             <option value="No">No</option>
                         </select>
                     </div>
                     <div class="form-col-1 question-container">
-                        <input type="text" placeholder="Question #5">
-                        <select name="" id="" required>
-                            <option value="Yes" selected hidden>Yes</option>
+                        <input class="question" id="questionNo5" name="questionNo5" type="text" placeholder="Question #5" required>
+                        <select name="" id="answerNo5" required>
+                            <option value="Choose" selected hidden>Choose</option>
                             <option value="Yes">Yes</option>
                             <option value="No">No</option>
                         </select>
                     </div>
                     <div class="form-col-2">
                         <div class="flex-button">
-                            <button class="btn-nav" step_number="3">Previous</button>
+                            <button class="btn-nav" step_number="2">Previous</button>
                         </div>
                         <div class="field-space"></div>
                         <div class="flex-button">
