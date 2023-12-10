9aa5c22078006a30f0867d27a118dcef774ed624
JN-025 <jncamposano025@gmail.com>
2023-12-10 23:39:01 +0800

    About PESO

---
+++ applicant/about_peso.php
@@ -0,0 +1,85 @@
+<?php
+$page_title = "Events";
+    
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../assets/css/about_peso.css">
+        <title>Laws</title>
+    </head>
+    <body>
+        
+        <div class="main-container">
+            <center>
+            <div class="container">
+                <h1>What is PESO?</h1>
+                
+                <div class="card1">
+                    <div class="box1">
+                        <h2>The Public Employment Service Office (PESO) is a non-fee charging multi-dimenstional employment service facility or entity established in all Local Government Units (LGUs) in coordination with the Department of Labor and Employment (DOLE) pursuant to R.A. No. 8759 or the PESO Act of 1999 as amended by R.A. No. 10691.</h2>
+                    </div>
+                    <div class="box2">
+                        <img src="../assets/img/peso.png" alt="PESO" class="img_peso">
+                    </div>
+
+                </div>
+
+                <div class="card2">
+                    <div class="box3">
+                        <center>
+                            <div class="box-card1">
+                                <h3>MISSION</h3>
+                            </div>
+                            <div class="box-card2" style="padding-top: 80px;">
+                                <h4>To ensure the speedy, equitable, and efficient employment services and to expand the existing employment facilitation services to maximize the impact of employment programs at the local government level</h4>
+                            </div>
+                        </center>
+                    </div>
+
+                    <div class="box3">
+                        <center>
+                            <div class="box-card1">
+                                <h3>VISION</h3>
+                            </div>
+                            <div class="box-card2" style="padding-top: 100px;">
+                                <h4>Inspired by diligent people who are committed to their work, we aim for excellence in everything that we do for the people of Santa Rosa and for our country.</h4>
+                            </div>
+                        </center>
+                    </div>
+
+                    <div class="box3">
+                        <center>
+                            <div class="box-card1">
+                                <h3>RATIONALE</h3>
+                            </div>
+                            <div class="box-card2" style="padding-top: 30px;">
+                                <h4>Employment is a basic Economic necessity of an individual, family, and the society. It measures the living standards and the developmental contribution to the national economy of the citizenry. Thus, employment services and facilitations thru PESO’s should be efficient, updated and relevant.</h4>
+                            </div>
+                        </center>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div class="container2">
+
+                
+            </div>
+            <div class="container3">
+                <div class="card3">
+                
+            </div>
+            </center>
+
+
+
+        </div>
+
+    </body>
+</html>
