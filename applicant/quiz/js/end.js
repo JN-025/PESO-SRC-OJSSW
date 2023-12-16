76f6f12b58fe23ace58b73320bbe86803e784e6b
rebo21 <joserebomanguerra@gmail.com>
2023-12-16 12:10:58 +0800

    updating game

--- applicant/quiz/js/end.js
+++ applicant/quiz/js/end.js
@@ -13,14 +13,14 @@ if (mostRecentScore >= 0 && mostRecentScore <= 20) {
 } else if (mostRecentScore >= 30 && mostRecentScore <= 40) {
     finalScore.style.color ="#CA9716";
     resultTitle.innerText = "Well Done";
-    resultHeader.innerText = "Learn from this mistakes and try again";
-    resultHeaderContent.innerText = "Congratulations on giving it your best, even in the face of challenges. Learn from this, and I'm confident you'll do even better next time. Keep going";
+    resultHeader.innerText = "You did well! Keep up";
+    resultHeaderContent.innerText = "Well done on your efforts! Taking the initiative and making the effort is a commendable step towards success. Keep pushing forward, and your determination will surely lead to great things";
     resultImage.src = "../../assets/img/scoremedium.png";
 } else if (mostRecentScore == 50) {
     finalScore.style.color ="#7EE100";
     resultTitle.innerText = "CONGRATULATIONS!";
-    resultHeader.innerText = "Learn from this mistakes and try again";
-    resultHeaderContent.innerText = "Congratulations on giving it your best, even in the face of challenges. Learn from this, and I'm confident you'll do even better next time. Keep going";
+    resultHeader.innerText = "You’re skills and knowledge exceeds you!";
+    resultHeaderContent.innerText = "Congratulations on a job well done! Your hard work, dedication, and achievements truly shine. Keep up the excellent work, and may your success continue to flourish!";
     resultImage.src = "../../assets/img/scorehigh.png";
 } else {
     resultTitle.innerText = "Error";
